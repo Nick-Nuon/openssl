a57be929fe37fb5d62767806de6955be24a83c90
Nick Nuon <myjpntrip@gmail.com>
2025-11-24 12:44:25 -0500

    remove runtime checks

--- crypto/evp/encode.c
+++ crypto/evp/encode.c
@@ -183,7 +183,7 @@ int EVP_EncodeUpdate(EVP_ENCODE_CTX *ctx, unsigned char *out, int *outl,
         j = evp_encodeblock_int(ctx, out, in, inl - (inl % ctx->length),
                                 &wrap_cnt);
     } else {
-#if defined(HAS_IA32CAP) && defined(__AVX2__)
+#if defined(__AVX2__)
         if ((OPENSSL_ia32cap_P[2] & (1u << 5)) != 0){
             const int newlines =
                 !(ctx->flags & EVP_ENCODE_CTX_NO_NEWLINES) ? ctx->length : 0;
@@ -243,7 +243,7 @@ int EVP_EncodeBlock(unsigned char *t, const unsigned char *f, int dlen)
 {
     int wrap_cnt = 0;
 
-#if defined(HAS_IA32CAP) && defined(__AVX2__)
+#if defined(__AVX2__)
     if ((OPENSSL_ia32cap_P[2] & (1u << 5)) != 0)
         return encode_base64_avx2(NULL, t, f, dlen, 0, &wrap_cnt);
     else
